7bc002b11292a2579900036758407541f943a8df
Camilo Roa <camiloroa@MacBook-Pro-de-Camilo.local>
2025-04-10 19:24:44 -0500

    fix: dependencia

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import axios from "axios";
+import crypto from "crypto";
 import { CreatePaymentDto } from "./dto/create-payment.dto";
 import {
   TokenCreationException,
